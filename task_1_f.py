b8605415b18719e0f89b7614625fa27fe08bc178
Disha6711 <141581582+Disha6711@users.noreply.github.com>
2025-01-18 00:54:49 +0530

    task_1_f.py

--- task_1_f.py
+++ task_1_f.py
@@ -1,21 +1,18 @@
 import requests
 
-# Function to fetch and display user data from the JSONPlaceholder API
 def fetch_user_data():
     try:
-        # Send GET request to the API
+        
         response = requests.get("https://jsonplaceholder.typicode.com/users")
         
-        # Check if the request was successful
         if response.status_code == 200:
-            users = response.json()  # Parse the JSON response
+            users = response.json()  
             
-            # Display specific information for each user
             print("User Details:\n")
             for user in users:
-                # Display 'Akanksha' as the name
-                if user['name'] == 'Leanne Graham':  # Replace this with any other condition if you need
-                    print(f"Name: Akanksha")
+                
+                if user['name'] == 'Leanne Graham':  
+                    print(f"Name: Disha Gareja")
                 else:
                     print(f"Name: {user['name']}")
                 print(f"Email: {user['email']}")
@@ -26,5 +23,4 @@ def fetch_user_data():
     except requests.exceptions.RequestException as e:
         print("An error occurred:", e)
 
-# Call the function to fetch and display user data
 fetch_user_data()
